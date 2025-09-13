80cbf375c0feedd84d8630a09b43477512b3984f
XercoDylan <xerco@mit.edu>
2025-09-13 13:55:31 -0400

    fixed gitignore

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+from waitress import serve
+from claud import *
+from wolfram import *
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
