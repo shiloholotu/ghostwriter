26844bbe9aae5e360b1b95bd1c3f76e6b30c783a
Shiloh Olotu <shiloholotu@gmail.com>
2025-09-13 14:44:58 -0400

    fix merge conflicts

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from waitress import serve
-#from claude import *
-#from wolfram import *
+from claude import *
+from wolfram import *
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,4 +10,6 @@ def home():
 
 
 if __name__ == "__main__":
+    print(request_wolf("generate me a one pragraph essay"))
+    ##app.run(debug=True)
     app.run(debug=True)
