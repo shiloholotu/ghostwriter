16e4bb1d7708f49af6fcce685cd2423df0a412ce
Ajibola Ajani <ajaniajibola469@gmail.com>
2025-09-13 14:24:42 -0400

    Move claude code to function

--- claude.py
+++ claude.py
@@ -7,11 +7,16 @@ client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key=api_key,
 )
-message = client.messages.create(
-    model="claude-sonnet-4-20250514",
-    max_tokens=1024,
-    messages=[
-        {"role": "user", "content": "Hello, Claude"}
-    ]
-)
-print(message.content)
+
+def prompt_claude(prompt):
+    response = client.messages.create(
+        model="claude-sonnet-4-20250514",
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Answer the following text without any additional commentary." + prompt
+            }
+        ]
+    )
+    return response.content
